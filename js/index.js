512148b9eec3c9af4dd5cff9d08ee9bf7db96d92
William Adams <wadams.dev@icloud.com>
2022-07-06 22:00:34 -0400

    Implemented filter by color

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
-console.log("Hello, World!");
+function blueFilter() {
+  var checkbox = document.getElementById("blueColor");
+  var text = document.getElementsByClassName("blue-item");
+  var items = document.getElementsByClassName("item");
+
+  if (checkbox.checked == true) {
+    for (let i = 0; i < items.length; i++) {
+      items[i].style.display = "none";
+    }
+    for (let i = 0; i < text.length; i++) {
+      text[i].style.display = "inline-block";
+    }
+  } else {
+    for (let i = 0; i < items.length; i++) {
+      items[i].style.display = "inline-block";
+    }
+  }
+}
+
+function yellowFilter() {
+  var checkbox = document.getElementById("yellowColor");
+  var text = document.getElementsByClassName("yellow-item");
+  var items = document.getElementsByClassName("item");
+
+  if (checkbox.checked == true) {
+    for (let i = 0; i < items.length; i++) {
+      items[i].style.display = "none";
+    }
+    for (let i = 0; i < text.length; i++) {
+      text[i].style.display = "inline-block";
+    }
+  } else {
+    for (let i = 0; i < items.length; i++) {
+      items[i].style.display = "inline-block";
+    }
+  }
+}
+
+function greenFilter() {
+  var checkbox = document.getElementById("greenColor");
+  var text = document.getElementsByClassName("green-item");
+  var items = document.getElementsByClassName("item");
+
+  if (checkbox.checked == true) {
+    for (let i = 0; i < items.length; i++) {
+      items[i].style.display = "none";
+    }
+    for (let i = 0; i < text.length; i++) {
+      text[i].style.display = "inline-block";
+    }
+  } else {
+    for (let i = 0; i < items.length; i++) {
+      items[i].style.display = "inline-block";
+    }
+  }
+}
